00f976231ca6967d8a2d6065abd4d3001b256669
Otavio <oaschwenk@minha.fag.edu.br>
2026-05-12 22:02:56 -0300

    feature: aula09

---
+++ Alunos/Otávio A. Schwenk/Aula09/Exceptions.java
@@ -0,0 +1,20 @@
+class DivisaoPorZeroException extends Exception {
+
+    public DivisaoPorZeroException() {
+        super("Não é possível dividir por zero!");
+    }
+}
+
+class SixSevenException extends Exception {
+
+    public SixSevenException() {
+        super("SIX SEVEEEEEN");
+    }
+}
+
+class KiraException extends Exception {
+
+    public KiraException() {
+        super("Killer Queen - Terceira Bomba: Bites The Dust!");
+    }
+}
